f3b3289a7cb4a639ae632140dc575f6d74b84b7c
karanbojappa7 <63574624+karanbojappa7@users.noreply.github.com>
2023-01-06 16:37:26 +0530

    /

--- files/scripts/main.js
+++ files/scripts/main.js
@@ -1,3 +1,16 @@
+ function openFunction(){
+  document.getElementById("menu").style.width="300px";
+  document.getElementById("mainbox").style.marginRight="0px";
+  document.getElementById("mainbox").innerHTML="";
+ }
+function closeFunction(){
+ document.getElementById("menu").style.width="0px";
+ document.getElementById("mainbox").style.marginRight="0px";
+ document.getElementById("mainbox").innerHTML="&#9776; ";
+}
+
+
+
 $('#play-video').on('click', function(e){
   e.preventDefault();
   $('#video-overlay').addClass('open');
